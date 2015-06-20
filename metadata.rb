767497f4b297eb51d9ebb16e85af4424b209a68d
gary <gwleong@gmail.com>
2015-06-20 01:46:44 +0000

    udpated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'wgalera'
 maintainer       'Gary Leong'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email 'gwleong@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures wgalera'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.5'
 
 depends 'galera'
 depends 'apt', '>= 1.8.2'
